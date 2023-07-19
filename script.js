05b58c85150b1520d3084f6c4ad71d91c23cf934
ADITYAMODI2112 <adityamodi2112@gmail.com>
2023-07-19 10:59:46 +0530

    commit("commit");

---
+++ script.js
@@ -0,0 +1,9 @@
+const zoomableDiv = document.getElementById('zoomable-div');
+
+function zoomIn() {
+  zoomableDiv.classList.add('zoom-in');
+}
+
+function zoomOut() {
+  zoomableDiv.classList.add('zoom-out');
+}
